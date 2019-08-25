5350f65125e7b5906d9f9f960fc8530fdcaca776
kandi <13817291910@163.com>
2019-08-25 16:29:45 +0800

    change the project structure to maven project and web app to test

--- src/main/java/support/BeanDefinitionReader.java
+++ src/main/java/support/BeanDefinitionReader.java
@@ -5,7 +5,6 @@ import beans.BeanDefinition;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
